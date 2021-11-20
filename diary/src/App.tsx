53acaff7833d7916123f7831b4eceff0ea552d38
slobbie <haeru9410@gmail.com>
2021-11-20 20:24:52 +0900

    study

---
+++ diary/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Gnb from './componants/Gnb';
+import Calendar from './componants/Calendar';
+
+function App() {
+  return (
+    <div className="App">
+      <Gnb />
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
--- my-app/src/components/Auth.tsx
+++ my-app/src/components/Auth.tsx
@@ -37,22 +37,3 @@ const users: Users[] = [
 
 
 
-//  interface User {
-//     email : string | Number;
-//     password : string | Number;
-//     name : string;
-//  }
-//   const users: User[] = [
-//     { email: "kim@test.com", password: "123", name: "Kim" },
-//     { email: "lee@test.com", password: "456", name: "Lee" },
-//     { email: "park@test.com", password: "789", name: "Park" },
-//   ];
-
-//   export function signIn(this:User, email:string | Number ,password:string | Number) {
-      
-//     const user = users.find(
-//       (user) => user.email === email && user.password === password
-//     );
-//     if (user === undefined) throw new Error();
-//     return user;
-//   }
