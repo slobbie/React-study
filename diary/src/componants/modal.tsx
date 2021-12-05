e2624b9dc7422d804e8f4cc2b4e430c3029c77f7
slobbie <haeru9410@gmail.com>
2021-12-06 07:11:48 +0900

    study

--- diary/src/componants/modal.tsx
+++ diary/src/componants/modal.tsx
@@ -3,13 +3,24 @@ import Calendar from './Calendar'
 
 
 const Modal = ({modalClose}: any) => {
+    const onCloseModal = (e: React.MouseEvent) => {
+        console.log('e.target: ', e.target)
+        console.log('e.tarcurrentTargetget: ', e.currentTarget)
+        if(e.target === e.currentTarget){
+            modalClose()
+        }
+
+    }
     return (
-        <div className="ModalContainer">
+        <div className="ModalContainer" onClick={onCloseModal}>
             <div className="modal">
-                <button onClick={modalClose}> Modal Close</button>
-                <div className="txet_title"></div>
-                <div className="txet_box"></div>
-                <input type="text" />
+                <button className="cloes_btn" onClick={modalClose}>X</button>
+                <div className="txet_title">
+                </div>
+                <div className="txet_box">
+                    <textarea maxLength={500} />
+                </div>
+                
             </div>
         </div>
     )
