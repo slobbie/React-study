832eb33c0b43fe1dc332d3987c3fa64dd2faf492
slobbie <haeru9410@gmail.com>
2021-12-05 23:41:26 +0900

    study

--- diary/src/componants/modal.tsx
+++ diary/src/componants/modal.tsx
@@ -7,6 +7,8 @@ const Modal = ({modalClose}: any) => {
         <div className="ModalContainer">
             <div className="modal">
                 <button onClick={modalClose}> Modal Close</button>
+                <div className="txet_title"></div>
+                <div className="txet_box"></div>
                 <input type="text" />
             </div>
         </div>
