a2987844fa3c9bd6714f92e6d44dbf08793aaaf0
slobbie <haeru9410@gmail.com>
2021-12-19 23:34:25 +0900

    study

---
+++ react-redux/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/init.scss';
+
+function App() {
+  return (
+    <div className="App">
+          <div className="a">안녕</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-redux/src/components/Todo.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Todo = ({onClick, completed, text }) => {
+  <li
+  onClick={onClick}
+  style={ {
+      textDecoration: completed ? 'line-throuhg' : 'none'
+  }}
+  >
+    {text}
+  </li>
+}
+
+Todo.propTypes = {
+    onclick: PropTypes.func.isRequired,
+    completed: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired
+}
+
+export default Todo;
