0a37a205bb019774a06997a097554553bf129de8
slobbie <haeru9410@gmail.com>
2022-02-14 03:07:37 +0900

    styled 중복 컴포넌트 줄이기

--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -4,26 +4,25 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
+
+const Circle = styled(Box)`
+  // styled(Box) box 컴포넌트의 속성을 계승 시키는 개념
+  border-radius: 50px;
 `;
+
 const Text = styled.span`
   color: white;
 `;
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor='teal'></Box>
+      <Circle bgColor='tomato' />
     </Father>
   );
 }
